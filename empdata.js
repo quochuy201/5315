cec0c939a92842e89c5682b4ba75bedeb8023fa1
harry <lehuy@seattleu.edu>
2019-04-19 19:58:53 -0700

    hw 3

--- empdata.js
+++ empdata.js
@@ -1,124 +1,109 @@
 {
   "data": [
-    {
-      "name": "James Bond",
-      "designation": "Accountant",
-      "salary": "$170,750",
-      "joining_date": "2011/07/25",
-      "office": "Tokyo",
-      "extension": "8422"
-    },
-    {
-      "name": "Brielle Williamson",
-      "designation": "Integration Specialist",
-      "salary": "$372,000",
-      "joining_date": "2012/12/02",
-      "office": "New York",
-      "extension": "4804"
-    },
-    {
-      "name": "Ashton Cox",
-      "designation": "Junior Technical Author",
-      "salary": "$86,000",
-      "joining_date": "2009/01/12",
-      "office": "San Francisco",
-      "extension": "1562"
-    },
-    {
-      "name": "Airi Satou",
-      "designation": "Accountant",
-      "salary": "$162,700",
-      "joining_date": "2008/11/28",
-      "office": "Tokyo",
-      "extension": "5407"
-    },
-    {
-      "name": "Caesar Vance",
-      "designation": "Pre-Sales Support",
-      "salary": "$106,450",
-      "joining_date": "2011/12/12",
-      "office": "New York",
-      "extension": "8330"
-    },
-    {
-      "name": "Shad Decker",
-      "designation": "Regional Director",
-      "salary": "$183,000",
-      "joining_date": "2008/11/13",
-      "office": "Edinburgh",
-      "extension": "6373"
-    },
-    {
-      "name": "Cedric Kelly",
-      "designation": "Senior Javascript Developer",
-      "salary": "$433,060",
-      "joining_date": "2012/03/29",
-      "office": "Edinburgh",
-      "extension": "6224"
-    },
-    {
-      "name": "Haley Kennedy",
-      "designation": "Senior Marketing Designer",
-      "salary": "$313,500",
-      "joining_date": "2012/12/18",
-      "office": "London",
-      "extension": "3597"
-    },
-    {
-      "name": "Colleen Hurst",
-      "designation": "Javascript Developer",
-      "salary": "$205,500",
-      "joining_date": "2009/09/15",
-      "office": "San Francisco",
-      "extension": "2360"
-    },
-    {
-      "name": "Dai Rios",
-      "designation": "Personnel Lead",
-      "salary": "$217,500",
-      "joining_date": "2012/09/26",
-      "office": "Edinburgh",
-      "extension": "2290"
-    },
-    {
-      "name": "Herrod Chandler",
-      "designation": "Sales Assistant",
-      "salary": "$137,500",
-      "joining_date": "2012/08/06",
-      "office": "San Francisco",
-      "extension": "9608"
-    },
-    {
-      "name": "Rhona Davidson",
-      "designation": "Integration Specialist",
-      "salary": "$327,900",
-      "joining_date": "2010/10/14",
-      "office": "Tokyo",
-      "extension": "6200"
-    },
-    {
-      "name": "Sonya Frost",
-      "designation": "Software Engineer",
-      "salary": "$103,600",
-      "joining_date": "2008/12/13",
-      "office": "Edinburgh",
-      "extension": "1667"
-    },
-    {
-      "name": "Jena Gaines",
-      "designation": "Office Manager",
-      "salary": "$90,560",
-      "joining_date": "2008/12/19",
-      "office": "London",
-      "extension": "3814"
-    },
-    {
-      "name": "Quinn Flynn",
-      "designation": "Support Lead",
-      "salary": "$342,000",
-      "joining_date": "2013/03/03",
-      "office": "Edinburgh",
-      "extension": "9497"
-    }
+       {
+      "fname": "Elise",
+      "lname": "Wilkins",
+      "gender": "female",
+      "designation": "Sales Support",
+      "salary": "$263,685"
+   },
+      {
+      "fname": "Brooks",
+      "lname": "Norman",
+      "gender": "female",
+      "designation": "IT Helpdesk",
+      "salary": "$126,982"
+   },
+      {
+      "fname": "Benson",
+      "lname": "Kinney",
+      "gender": "female",
+      "designation": "Sales Support",
+      "salary": "$330,311"
+   },
+      {
+      "fname": "Hope",
+      "lname": "Drake",
+      "gender": "male",
+      "designation": "IT Staff",
+      "salary": "$334,653"
+   },
+      {
+      "fname": "Stacy",
+      "lname": "Mack",
+      "gender": "male",
+      "designation": "IT Helpdesk",
+      "salary": "$481,861"
+   },
+      {
+      "fname": "Eliana",
+      "lname": "Frye",
+      "gender": "male",
+      "designation": "Consultant",
+      "salary": "$164,819"
+   },
+      {
+      "fname": "Janelle",
+      "lname": "Klein",
+      "gender": "female",
+      "designation": "Consultant",
+      "salary": "$460,420"
+   },
+      {
+      "fname": "Jenna",
+      "lname": "Tillman",
+      "gender": "female",
+      "designation": "Consultant",
+      "salary": "$130,748"
+   },
+      {
+      "fname": "Evelynn",
+      "lname": "Ashley",
+      "gender": "female",
+      "designation": "Sales",
+      "salary": "$233,360"
+   },
+      {
+      "fname": "Ciara",
+      "lname": "Weaver",
+      "gender": "female",
+      "designation": "IT Staff",
+      "salary": "$74,985"
+   },
+      {
+      "fname": "Irvin",
+      "lname": "Owens",
+      "gender": "male",
+      "designation": "IT Helpdesk",
+      "salary": "$70,116"
+   },
+      {
+      "fname": "Yuliana",
+      "lname": "Foreman",
+      "gender": "male",
+      "designation": "IT Staff",
+      "salary": "$53,492"
+   },
+      {
+      "fname": "Anna",
+      "lname": "Sykes",
+      "gender": "male",
+      "designation": "Director",
+      "salary": "$483,232"
+   },
+      {
+      "fname": "Jamie",
+      "lname": "Floyd",
+      "gender": "female",
+      "designation": "Recruiter",
+      "salary": "$175,087"
+   },
+      {
+      "fname": "Brysen",
+      "lname": "Vincent",
+      "gender": "male",
+      "designation": "Director",
+      "salary": "$281,789"
+   }
   ]
 }
