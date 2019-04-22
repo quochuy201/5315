5cae756f303e6fb36e82eabfb9cfb321854893e9
harry <lehuy@seattleu.edu>
2019-04-21 19:26:16 -0700

    hw3 update

--- empdata.js
+++ empdata.js
@@ -1,109 +1,204 @@
 {
   "data": [
-       {
-      "fname": "Elise",
-      "lname": "Wilkins",
-      "gender": "female",
-      "designation": "Sales Support",
-      "salary": "$263,685"
+      {
+      "SUID": 400000,
+      "name": "Reyna Carey",
+      "dob": "1991-12-26",
+      "program": "MPAC",
+      "course": "ECON5100",
+      "grade": 85
    },
       {
-      "fname": "Brooks",
-      "lname": "Norman",
-      "gender": "female",
-      "designation": "IT Helpdesk",
-      "salary": "$126,982"
+      "SUID": 400001,
+      "name": "Ismael Page",
+      "dob": "1986-02-12",
+      "program": "PMBA",
+      "course": "ECON5310",
+      "grade": 75
    },
       {
-      "fname": "Benson",
-      "lname": "Kinney",
-      "gender": "female",
-      "designation": "Sales Support",
-      "salary": "$330,311"
+      "SUID": 400002,
+      "name": "Gemma Young",
+      "dob": "1989-07-07",
+      "program": "MSF",
+      "course": "MATH5170",
+      "grade": 96
    },
       {
-      "fname": "Hope",
-      "lname": "Drake",
-      "gender": "male",
-      "designation": "IT Staff",
-      "salary": "$334,653"
+      "SUID": 400003,
+      "name": "Edwin Stanley",
+      "dob": "1989-09-14",
+      "program": "MSF",
+      "course": "IS5315",
+      "grade": 73
    },
       {
-      "fname": "Stacy",
-      "lname": "Mack",
-      "gender": "male",
-      "designation": "IT Helpdesk",
-      "salary": "$481,861"
+      "SUID": 400004,
+      "name": "Mattie Case",
+      "dob": "1992-04-28",
+      "program": "MSF",
+      "course": "BUAN5310",
+      "grade": 92
    },
       {
-      "fname": "Eliana",
-      "lname": "Frye",
-      "gender": "male",
-      "designation": "Consultant",
-      "salary": "$164,819"
+      "SUID": 400005,
+      "name": "Vaughn Molina",
+      "dob": "1985-10-10",
+      "program": "MPAC",
+      "course": "BUAN5210",
+      "grade": 86
    },
       {
-      "fname": "Janelle",
-      "lname": "Klein",
-      "gender": "female",
-      "designation": "Consultant",
-      "salary": "$460,420"
+      "SUID": 400006,
+      "name": "Isis Page",
+      "dob": "1988-09-26",
+      "program": "MPAC",
+      "course": "MATH5170",
+      "grade": 52
    },
       {
-      "fname": "Jenna",
-      "lname": "Tillman",
-      "gender": "female",
-      "designation": "Consultant",
-      "salary": "$130,748"
+      "SUID": 400007,
+      "name": "Brennan Mason",
+      "dob": "1985-04-18",
+      "program": "MSBA",
+      "course": "MATH5170",
+      "grade": 62
    },
       {
-      "fname": "Evelynn",
-      "lname": "Ashley",
-      "gender": "female",
-      "designation": "Sales",
-      "salary": "$233,360"
+      "SUID": 400008,
+      "name": "Juliet Griffin",
+      "dob": "1988-03-20",
+      "program": "MPAC",
+      "course": "IS5315",
+      "grade": 91
    },
       {
-      "fname": "Ciara",
-      "lname": "Weaver",
-      "gender": "female",
-      "designation": "IT Staff",
-      "salary": "$74,985"
+      "SUID": 400009,
+      "name": "Lailah Hill",
+      "dob": "1992-01-10",
+      "program": "MSBA",
+      "course": "MATH5170",
+      "grade": 53
    },
       {
-      "fname": "Irvin",
-      "lname": "Owens",
-      "gender": "male",
-      "designation": "IT Helpdesk",
-      "salary": "$70,116"
+      "SUID": 400010,
+      "name": "Micah Vasquez",
+      "dob": "1993-09-10",
+      "program": "MSBA",
+      "course": "ECON5100",
+      "grade": 93
    },
       {
-      "fname": "Yuliana",
-      "lname": "Foreman",
-      "gender": "male",
-      "designation": "IT Staff",
-      "salary": "$53,492"
+      "SUID": 400011,
+      "name": "Mckayla Ortega",
+      "dob": "1986-04-08",
+      "program": "PMBA",
+      "course": "MATH5170",
+      "grade": 66
    },
       {
-      "fname": "Anna",
-      "lname": "Sykes",
-      "gender": "male",
-      "designation": "Director",
-      "salary": "$483,232"
+      "SUID": 400012,
+      "name": "Hendrix Taylor",
+      "dob": "1994-08-22",
+      "program": "MSF",
+      "course": "BUAN5310",
+      "grade": 56
    },
       {
-      "fname": "Jamie",
-      "lname": "Floyd",
-      "gender": "female",
-      "designation": "Recruiter",
-      "salary": "$175,087"
+      "SUID": 400013,
+      "name": "Davon Young",
+      "dob": "1988-10-10",
+      "program": "MPAC",
+      "course": "MATH5170",
+      "grade": 72
    },
       {
-      "fname": "Brysen",
-      "lname": "Vincent",
-      "gender": "male",
-      "designation": "Director",
-      "salary": "$281,789"
+      "SUID": 400014,
+      "name": "Anabella Whitaker",
+      "dob": "1990-05-28",
+      "program": "PMBA",
+      "course": "ECON5310",
+      "grade": 88
+   },
+      {
+      "SUID": 400015,
+      "name": "Soren Dennis",
+      "dob": "1986-10-06",
+      "program": "MSF",
+      "course": "MATH5170",
+      "grade": 68
+   },
+      {
+      "SUID": 400016,
+      "name": "Mara Spence",
+      "dob": "1992-06-22",
+      "program": "MSBA",
+      "course": "ECON5100",
+      "grade": 62
+   },
+      {
+      "SUID": 400017,
+      "name": "Eloise Pena",
+      "dob": "1985-11-21",
+      "program": "MSBA",
+      "course": "BUAN5260",
+      "grade": 98
+   },
+      {
+      "SUID": 400018,
+      "name": "Jaylyn Rodgers",
+      "dob": "1989-07-19",
+      "program": "MSBA",
+      "course": "ECON5100",
+      "grade": 75
+   },
+      {
+      "SUID": 400019,
+      "name": "Vera Leonard",
+      "dob": "1991-04-30",
+      "program": "MSBA",
+      "course": "ECON5310",
+      "grade": 97
+   },
+      {
+      "SUID": 400020,
+      "name": "Jaxen Bryan",
+      "dob": "1988-08-05",
+      "program": "PMBA",
+      "course": "BUAN5310",
+      "grade": 88
+   },
+      {
+      "SUID": 400021,
+      "name": "Jean Osborn",
+      "dob": "1989-08-13",
+      "program": "MPAC",
+      "course": "MATH5170",
+      "grade": 100
+   },
+      {
+      "SUID": 400022,
+      "name": "Aniya Weber",
+      "dob": "1987-08-24",
+      "program": "MSBA",
+      "course": "IS5315",
+      "grade": 90
+   },
+      {
+      "SUID": 400023,
+      "name": "Delilah Parsons",
+      "dob": "1989-05-27",
+      "program": "MSBA",
+      "course": "ECON5100",
+      "grade": 84
+   },
+      {
+      "SUID": 400024,
+      "name": "Lauryn Lester",
+      "dob": "1985-11-07",
+      "program": "MSF",
+      "course": "IS5315",
+      "grade": 94
    }
-  ]
+]
 }
